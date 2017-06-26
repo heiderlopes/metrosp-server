a4582dd4cc947f4a92b851cfcfc5ffa63c6f6818
Heider Lopes <heidertreinamentos@gmail.com>
2017-06-26 17:07:31 -0300

    Inclusao de duas estacoes da linha azul

--- azul.js
+++ azul.js
@@ -11,11 +11,25 @@ router.get('/azul/estacoes', function(req, res) {
   res.send(ESTACOES_LINHA_AZUL);
 });
 
-const ESTACOES_LINHA_AZUL = [{
-	"nome" : "JABAQUARA",
-	"endereco" : "RUA DOS JEQUITIBÁS, 80",
-	"latitude" : "-23.6462810",
-	"longitude" : "-46.6425470",
-}];
+const ESTACOES_LINHA_AZUL = [
+    {
+        "nome" : "JABAQUARA",
+        "endereco" : "RUA DOS JEQUITIBÁS, 80",
+        "latitude" : "-23.6462810",
+        "longitude" : "-46.6425470",
+        "capacidade_passageiro_hora_pico" : "20.000",
+        "area_construida_m_2" : 6850,
+        "inauguracao" : "14/09/1974"
+    },
+    {
+        "nome" : "CONCEIÇÃO",
+        "endereco" : "AV. ENG. ARMANDO DE ARRUDA PEREIRA, 919",
+        "latitude" : "-23.6349220",
+        "longitude" : "-46.6411180",
+        "capacidade_passageiro_hora_pico" : "20.000",
+        "area_construida_m_2" : 6850,
+        "inauguracao" : "14/09/1974"
+    }
+];
 
 module.exports = router;
